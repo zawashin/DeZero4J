276801e1bd971c95395a840b7ecb4adb813f8085
zawashin <zawashin@users.noreply.github.com>
2024-07-04 12:24:28 +0900

    Delete src/main/java/dezer4j/step directory

--- src/main/java/dezer4j/step/step03/AbstractFunction.java
+++
@@ -1,8 +0,0 @@
-package dezero4j.step.step03;
-
-public abstract class AbstractFunction {
-    public abstract Variable forward(Variable input);
-
-    protected abstract double forward(double x);
-}
-
