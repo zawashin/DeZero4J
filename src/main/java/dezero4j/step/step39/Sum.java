97418af04d59e9517ad89540d4382505b0fd5c3e
zawashin <zawashin@outlook.com>
2024-11-06 15:25:17 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/dezero4j/step/step39/Sum.java
+++ src/main/java/dezero4j/step/step39/Sum.java
@@ -3,6 +3,8 @@ package dezero4j.step.step39;
 import tensor4j.Tensor;
 import tensor4j.Utils;
 
+import java.util.Arrays;
+
 /**
  * @author Shin-Ichiro Serizawa <zawashin@outlook.com>
  */
@@ -13,11 +15,11 @@ public class Sum extends Function {
     private final boolean keepDims;
 
     public Sum() {
-        this(-1, true);
+        this(-1, false);
     }
 
     public Sum(int axis) {
-        this(axis, true);
+        this(axis, false);
     }
 
     public Sum(int axis, boolean keepDims) {
@@ -34,24 +36,42 @@ public class Sum extends Function {
 
     @Override
     public Variable[] backward(Variable... gys) {
-        Tensor gy = Utils.reshapeSumBackward(gys[0].getData(), xShape, axis, keepDims);
-        Variable gx = new Variable(Utils.broadcastTo(gy, xShape));
+        Tensor gy = Utils.reshapeSumBackward(gys[0].getData(), xShape, keepDims, axis);
+        //Variable gx = new Variable(Utils.broadcastTo(gy, xShape));
+        Variable gx = new Variable(gy);
         return new Variable[]{gx};
     }
 
     public static void main(String[] args) {
         Variable[] xs = new Variable[1];
         Variable x;
-        Variable y;
-        x = new Variable(new double[][]{{1, 2, 3}, {4, 5, 6}});
-        xs[0] = x;
-        System.out.println("x");
+        Tensor tensor;
+        int n;
+        System.out.println("1D");
+        double[] vector = new double[5];
+        tensor = new Tensor(vector);
+        n = 0;
+        for (int i = 0; i < tensor.getLength(); i++) {
+            tensor.getValues()[n] = n++;
+        }
+        x = new Variable(tensor);
         System.out.println(x);
-        Function f = new Sum();
-        y = f.forward(xs)[0];
-        System.out.println("y");
-        System.out.println("axis *:");
-        System.out.println(y);
+        System.out.println(x.sum());
+
+        double[][] matrix = new double[3][5];
+        System.out.println("2D");
+        tensor = new Tensor(matrix);
+        n = 0;
+        for (int i = 0; i < tensor.getLength(); i++) {
+            tensor.getValues()[n] = n++;
+        }
+        x = new Variable(tensor);
+        System.out.println(x);
+        System.out.println(x.sum());
+        System.out.println(x.sum(0));
+        System.out.println(x.sum(1));
+        x = new Variable(new double[][]{{1, 2, 3}, {4, 5, 6}});
+        /*
         y.backward(false, true);
         System.out.println(x.grad);
         x.clearGrad();
@@ -69,6 +89,8 @@ public class Sum extends Function {
         x.clearGrad();
         y.backward(false, true);
         System.out.println(x.grad);
+
+         */
     }
 
 }
