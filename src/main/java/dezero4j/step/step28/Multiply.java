d99091766c30a611c328a3f51f835c38089e7f9a
zawashin <zawashin@outlook.com>
2024-12-17 01:15:57 +0900

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/dezero4j/step/step28/Multiply.java
@@ -0,0 +1,21 @@
+package dezero4j.step.step28;
+
+import tensor4j.Tensor;
+
+/**
+ * @author Shin-Ichiro Serizawa <zawashin@outlook.com>
+ */
+public class Multiply extends Function {
+
+    @Override
+    public Tensor[] forward(Tensor[] xs) {
+        return new Tensor[]{xs[0].multiply(xs[1])};
+    }
+
+    @Override
+    public Tensor[] backward(Tensor[] gys) {
+        Tensor[] xs = new Tensor[]{inputs[0].getData(), inputs[1].getData()};
+        return new Tensor[]{xs[1].multiply(gys[0]), xs[0].multiply(gys[0])};
+    }
+
+}
