a7e1cc1a96902fb5fc6331f8ba150067f3f5f680
zawashin <zawashin@outlook.com>
2025-01-11 16:08:21 +0900

    Merge remote-tracking branch 'origin/master' remote-tracking branch 'origin/master'

--- src/main/java/step/step48/Step48.java
+++ src/main/java/step/step48/Step48.java
@@ -21,7 +21,9 @@ public class Step48 extends Step {
         spiral.generateSpiral(true);
         int max_epoch = 10000;
         int batch_size = 1;
-        Variable x = new Variable(spiral.getX());
+        double[][] xy = spiral.getX();
+        int[] labels = spiral.getTarget();
+        Variable x = new Variable(xy);
         // int[]をdouble[]に変換
         Variable t = new Variable(Arrays.stream(spiral.getTarget()).asDoubleStream().toArray());
         Model model = new TwoLayerNet(10, 3);
@@ -29,7 +31,7 @@ public class Step48 extends Step {
         Optimizer optimizer = new SGD(model, 0.2);
         int data_size = x.getShape()[0];
         int iters = data_size / batch_size;
-        iters = 25000;
+        iters = 30000;
 
         Function f = new SoftmaxCrossEntropy();
 
@@ -38,11 +40,11 @@ public class Step48 extends Step {
             Variable loss = f.forward(y, t)[0];
             if (i % 1000 == 0) {
                 System.out.println("Loss[" + i + "] = " + loss);
-                System.gc();
             }
             if (loss.getValues()[0] < 0.05) {
                 break;
             }
+
             model.clearGrads();
             loss.backward(false, true);
 
@@ -52,5 +54,8 @@ public class Step48 extends Step {
         for (int i = 0; i < x.getShape()[0]; i++) {
             System.out.println(t.getValues()[i] + " " + te.getValues()[i]);
         }
+        Variable xi = new Variable(new double[][]{{0.5, 0.5}, {-0.5, -0.5}});
+        Variable yi = VariableUtils.softMaxArgMax(model.predict(xi));
+        System.out.println(xi + " " + yi);
     }
 }
